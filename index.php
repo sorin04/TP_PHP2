3597322788f17149258d461ae24e99f324f59329
spirgari <sorin.pirgari@lyceeastier.com>
2024-04-10 17:42:11 +0200

    1

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+class  Personne{
+     private $nom;
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+     public $prenom;
+
+    /**
+     * @return mixed
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @param mixed $prenom
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+    }
+     public $annee_naissance;
+
+     public function __construct(){
+
+     }
+
+
+}
